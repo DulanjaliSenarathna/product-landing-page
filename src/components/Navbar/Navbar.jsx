3594d0b90d0aca5dd5b2345287bb3caebe948bc0
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-10-19 19:42:55 +0530

    Navbar done for mobile and desktop views

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdMenu } from 'react-icons/md';
 import { FaBicycle } from "react-icons/fa";
+import ResponsiveMenu from './ResponsiveMenu';
+
 
 const NavbarMenu = [
     {
@@ -30,38 +32,57 @@ const NavbarMenu = [
     },
     {
         id:6,
-        title: "Call-to-Action",
+        title: "Contact Us",
         link: "#"
     }
 ]
 
+
+
 const Navbar = () => {
+   
+    const [open, setOpen] = useState(false);
+
+     const handleMenuOpen = () =>{
+        setOpen(!open);
+    }
+
   return (
-    <div>
+    <>
+    <nav>
+        <div className='container flex justify-between items-center py-4 md:pt-4 '>
         {/*________ Logo section ________*/}
-        <div>
-            <a href="#">
-                BIKE <span>HUB</span>
-            </a>
+        <div className='text-2xl flex items-center gap-2 font-bold uppercase'>
+               <p className='text-primary'>BIKE</p>
+               <p className='text-secondary'>HUB</p>
+               <FaBicycle className='text-green-500'/>  
         </div>
         {/*________ Menu section ________*/}
-        <div>
-            <ul>
+        <div className='hidden md:block'>
+            <ul className='flex items-center gap-6 text-gray-600'>
                 {
                     NavbarMenu.map((item)=>(
                         <li key={item.id}>
-                            <a href={item.link}>{item.title}</a>
+                            <a href={item.link}
+                            className='inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#ef4444] font-semibold'
+                            >{item.title}</a>
                         </li>
                     ))
                 }
-                <FaBicycle />
+                
             </ul>
         </div>
-        {/*________ Mobile Navbar ________*/}
-        <div>
-            <MdMenu/>
+
+        {/*________ Mobile Hamburger Menu section________*/}
+        <div className='md:hidden' onClick={handleMenuOpen}>
+            <MdMenu className='text-4xl'/>
         </div>
-    </div>
+        </div>
+    </nav>
+
+     {/*________ Mobile Hamburger Menu section________*/}
+     <ResponsiveMenu open={open}/>
+     </>
   )
 }
 
